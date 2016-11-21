ce5c1bc645f9fe3f205466db547e8f29b4e9dc85
Rohit Sthapit <nhuzaa@gmail.com>
2016-11-21 15:04:15 +0545

    added the start button

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
 var TIME_LOOP = 50; // Millisecond
 var CANVAS_WIDTH = 200;
 var CANVAS_HEIGHT = 500;
-var BLOCKADE_LIMIT = 4 ;
+var BLOCKADE_LIMIT = 4;
 
 
 var CAR_HEIGHT = 100;
@@ -24,7 +24,7 @@ var CAR_WIDTH = 60;
 var BLOCKADE_HEIGHT = 60
 var BLOCKADE_WIDTH = 60;
 
-var BLOCKADE_SPACING =1.75 ;
+var BLOCKADE_SPACING = 1.75;
 
 var BLOCKAGE_SPEED = 2;
 /**
@@ -32,57 +32,57 @@ var BLOCKAGE_SPEED = 2;
  */
 function Car() {
 
-    var posX; // Bottom
-    var posY; // Left
-    var state = 0; // 0 >> Left 1 >> Right
+  var posX; // Bottom
+  var posY; // Left
+  var state = 0; // 0 >> Left 1 >> Right
 
-    this.element ;
+  this.element;
 
-    /**
-     * Car Constructor
-     * @return {[type]} [description]
-     */
-    this.init = function() {
-        this.element = document.createElement('div');
-        this.element.setAttribute('class', 'car');
-        posX = 0;
-        posY = 0;
-        this.set(0);
-    }
+  /**
+   * Car Constructor
+   * @return {[type]} [description]
+   */
+  this.init = function () {
+    this.element = document.createElement( 'div' );
+    this.element.setAttribute( 'class', 'car' );
+    posX = 0;
+    posY = 0;
+    this.set( 0 );
+  }
 
-    /**
-     * Change the Left and Right State of Car
-     * @param {[Boolean]} key [ 0 >> Left 1 >> Right ]
-     */
-    this.set = function(key) {
-        state = key;
-        this.checkState();
-    }
+  /**
+   * Change the Left and Right State of Car
+   * @param {[Boolean]} key [ 0 >> Left 1 >> Right ]
+   */
+  this.set = function ( key ) {
+    state = key;
+    this.checkState();
+  }
 
-    this.checkState = function() {
-      var that = this;
-        if (state == 0) //LEFT
-        {
-            posX = 0;
-        } else if (state == 1) // RIGHT
-        {
-            posX = CANVAS_WIDTH / 2;
-
-        }
-
-      that.draw();
-    }
-
-    this.getState = function(){
-      return state;
+  this.checkState = function () {
+    var that = this;
+    if ( state == 0 ) //LEFT
+    {
+      posX = 0;
+    } else if ( state == 1 ) // RIGHT
+    {
+      posX = CANVAS_WIDTH / 2;
 
     }
 
-    this.draw = function() {
-        this.element.style.left = posX + 'px';
-        this.element.style.bottom = posY + 'px';
+    that.draw();
+  }
 
-    }
+  this.getState = function () {
+    return state;
+
+  }
+
+  this.draw = function () {
+    this.element.style.left = posX + 'px';
+    this.element.style.bottom = posY + 'px';
+
+  }
 }
 
 
@@ -93,57 +93,57 @@ function Car() {
 function Blockade() {
 
 
-    var state; // 0 >> LEFT  | 1 >> RIGHT
+  var state; // 0 >> LEFT  | 1 >> RIGHT
 
-    var color = "red"
-    var speed = 10;
+  var color = "red"
+  var speed = 10;
 
-    this.posX = 0; // Bottom
-    this.posY = 0; // Left
+  this.posX = 0; // Bottom
+  this.posY = 0; // Left
 
-    var blockadeId ;
-    this.element;
+  var blockadeId;
+  this.element;
 
-    /**
-     * Blockad Constructor
-     * @return {[type]} [description]
-     */
-    this.init = function(id) {
-        this.element = document.createElement('div');
-        this.element.setAttribute('class', 'blockade'); // div with class car
-        blockadeId = id;
-        this.set();
+  /**
+   * Blockad Constructor
+   * @return {[type]} [description]
+   */
+  this.init = function ( id ) {
+    this.element = document.createElement( 'div' );
+    this.element.setAttribute( 'class', 'blockade' ); // div with class car
+    blockadeId = id;
+    this.set();
+  }
+
+  this.set = function () {
+
+    state = getRandom( 0, 1 ); // LEFT | RIGHT
+
+    this.posY = blockadeId * BLOCKADE_SPACING * CAR_HEIGHT + 2 * CAR_HEIGHT;
+
+    if ( state == 0 ) {
+      this.posX = 0
+    } else if ( state == 1 ) {
+
+      this.posX = CANVAS_WIDTH / 2;
     }
 
-    this.set = function() {
+    this.draw();
+  }
 
-        state = getRandom(0, 1); // LEFT | RIGHT
+  this.getState = function () {
+    return state;
 
-        this.posY =  blockadeId * BLOCKADE_SPACING * CAR_HEIGHT +  2 * CAR_HEIGHT ;
+  }
 
-        if (state == 0 ){
-          this.posX = 0
-        }else if (state == 1){
+  this.draw = function () {
 
-          this.posX = CANVAS_WIDTH /2;
-        }
+    this.element.style.left = this.posX + 'px';
 
-        this.draw();
-    }
-
-    this.getState  = function(){
-      return state;
-
-    }
-
-    this.draw = function() {
-
-        this.element.style.left = this.posX + 'px';
-
-        this.element.style.bottom = this.posY + 'px';
+    this.element.style.bottom = this.posY + 'px';
 
 
-    }
+  }
 }
 
 
@@ -156,91 +156,89 @@ function Blockade() {
  * @return {[type]} [description]
  */
 function main() {
-    var blockades = [];
-    var container = document.getElementById('container');
+  var blockades = [];
+  var container = document.getElementById( 'container' );
 
-    // Creating Car
-    var car = new Car();
-    car.init(0, 0);
-    car.draw();
-    console.log("car drawn");
-    container.appendChild(car.element); // Adding bubble to the container
-
-
-    var keyPress = document.addEventListener("keydown", function (event) {
-
-      var c = event.keycode || event.which ;
-      switch (c) {
-        case 37:
-          car.set(0); //LEFT
-          break;
-
-        case 39:
-          car.set(1); // RIGHT
-          break
-        default:
-      };
-    });
+  // Creating Car
+  var car = new Car();
+  car.init( 0, 0 );
+  car.draw();
+  console.log( "car drawn" );
+  container.appendChild( car.element ); // Adding bubble to the container
 
 
-    /**
-     * Creating Blockades and adding it to the blockades array
-     * @return {[None]} [None]
-     */
-    var createBlockades = function() {
+  var keyPress = document.addEventListener( "keydown", function ( event ) {
 
-        for (var i = 0; i < BLOCKADE_LIMIT; i++) {
-            var blockade = new Blockade();
-            blockade.init(i);
-            console.log("blockade created" + i);
+    var c = event.keycode || event.which;
+    switch ( c ) {
+    case 37:
+      car.set( 0 ); //LEFT
+      break;
+
+    case 39:
+      car.set( 1 ); // RIGHT
+      break
+    default:
+    };
+  } );
+
+
+  /**
+   * Creating Blockades and adding it to the blockades array
+   * @return {[None]} [None]
+   */
+  var createBlockades = function () {
+
+    for ( var i = 0; i < BLOCKADE_LIMIT; i++ ) {
+      var blockade = new Blockade();
+      blockade.init( i );
+      console.log( "blockade created" + i );
 
 
 
-            blockades.push(blockade);
-            container.appendChild(blockade.element); // Adding bubble to the container
-        }
-
+      blockades.push( blockade );
+      container.appendChild( blockade.element ); // Adding bubble to the container
     }
 
-    /**
-     * Movement of Blockades
-     * @return {[None]} [None]
-     */
-     var moveBlockades= function() {
-        for (var i = 0; i < BLOCKADE_LIMIT; i++) {
-            var blockade = blockades[i];
-            if (blockade.posY > 0)
-            {
-              blockade.posY -= BLOCKAGE_SPEED;
-            }else{
-              blockade.posY  =  BLOCKADE_LIMIT * BLOCKADE_SPACING * CAR_HEIGHT +  2 * CAR_HEIGHT ;
-            }
+  }
 
-            if(blockade.getState() == car.getState() && blockade.posY <= CAR_HEIGHT)
-            {
-              console.log("Car" + car.getState()  + "blockade" + blockade.getState());
-              clearInterval(gameloop_var);
-            }
-            blockade.draw();
+  /**
+   * Movement of Blockades
+   * @return {[None]} [None]
+   */
+  var moveBlockades = function () {
+    for ( var i = 0; i < BLOCKADE_LIMIT; i++ ) {
+      var blockade = blockades[ i ];
+      if ( blockade.posY > 0 ) {
+        blockade.posY -= BLOCKAGE_SPEED;
+      } else {
+        blockade.posY = BLOCKADE_LIMIT * BLOCKADE_SPACING * CAR_HEIGHT + 2 * CAR_HEIGHT;
+      }
 
-        }
-    }
-
-    // ############### GAME LOOP ########################
-    var gameloop_var = setInterval(gameloop, TIME_LOOP);
-
-    /**
-     * Main Game Loop
-     * @return {[type]} [description]
-     */
-    function gameloop() {
-
-        moveBlockades();
-
-
+      if ( blockade.getState() == car.getState() && blockade.posY <= CAR_HEIGHT ) {
+        console.log( "Car" + car.getState() + "blockade" + blockade.getState() );
+        clearInterval( gameloop_var );
+      }
+      blockade.draw();
 
     }
-      createBlockades();
+  }
+
+  // ############### GAME LOOP ########################
+  var gameloop_var = setInterval( gameloop, TIME_LOOP );
+
+  /**
+   * Main Game Loop
+   * @return {[type]} [description]
+   */
+  function gameloop() {
+
+    moveBlockades();
+
+
+
+  }
+  createBlockades();
 
 }
 
@@ -250,9 +248,15 @@ function main() {
  * @param  {[Integer]} max [description]
  * @return {[Random Value]}     [description]
  */
-function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function getRandom( min, max ) {
+  return Math.floor( Math.random() * ( max - min + 1 ) + min );
 }
 
 // Initializing
-main();
+
+var start = document.getElementById( 'start' );
+
+start.addEventListener( 'click', function () {
+  main();
+
+} )
